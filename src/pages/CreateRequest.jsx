dd2080f5907f5a6a447c8bb7529ccdca33ff50fd
bugsyy157 <it.b.60.abhishek.yadav@gmail.com>
2022-10-23 01:11:21 +0530

    add

--- src/pages/CreateRequest.jsx
+++ src/pages/CreateRequest.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./creatreq.css";
 
 function CreateRequest() {
    
@@ -6,23 +7,29 @@ function CreateRequest() {
     return (
       <>
       <h1>Create Request</h1><br />
-      <div>
+      <div className='requestdiv'>
       <form>
-        <label>Select Product:
-          <select
-            
-          >
+        <label>Select Product : </label>
+          <select>
+          <option value="opt">Select Product to Add</option>
            <option value="wheat">Wheat</option>
            <option value="rice">Rice</option>
            <option value="corn">Corn</option>
            <option value="Maize">Maize</option>
           </select>
-        </label><br /><br /><br />
+        <br /><br /><br />
         <label htmlFor="quantity">Quantity : </label>
-        <input type="text" />
+
+        <input type="number" id="quantity" name="quantity" min="1"  />
+        <select>
+        <option value="unit">Units</option>
+           <option value="kg">kgs</option>
+           <option value="quintal">Quintal</option>
+        </select>
+
         <br /><br />
          <label htmlFor="cost">Government issued price: </label>
-         <input type="text" readOnly /><br /><br />
+         <input type="number" readOnly /><br /><br />
          <button>SUBMIT</button>
       </form>
       </div>
