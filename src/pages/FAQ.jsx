7e5b339ffa978a1f6a6c0475b2bd74b493b01517
Pranit-02 <pranitutekar1234@gmail.com>
2022-10-28 11:49:09 +0530

    Added FAQ section and modified css

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,11 +1,65 @@
+import { useState } from 'react';
 import React from 'react';
 
 const FAQ = () => {
+    const [selected, setSelected] = useState(null)
+
+    const toggle = i => {
+        if (selected == i) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+
     return (
-        <div>
-            <h1>FAQS</h1>
+        <>
+        <h1>FAQ</h1>
+        <div className='wrapper'>
+            <div className="accordian">
+
+                {data.map((item, i) => (
+                    <div className="item">
+                        <div className="title" onClick={() => toggle(i)}>
+                            <h2>{item.question}</h2>
+                            <span>{selected == i ? '-' : '+' }</span>
+                        </div>
+                        <div className={selected == i ? 'content show' : 'content' }>{item.answer}</div>
+                    </div>
+                ))}
+            </div>
         </div>
+        </>
     );
 };
+
+const data = [
+    {
+        question: 'Question 1',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda aspernatur, eligendi ipsam ratione natus ea cumque voluptatibus mollitia laborum, ab itaque ipsa qui suscipit a.Vel, totam error.Voluptatem, eum?.',
+    },
+    {
+        question: 'Question 2',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda aspernatur, eligendi ipsam ratione natus ea cumque voluptatibus mollitia laborum, ab itaque ipsa qui suscipit a.Vel, totam error.Voluptatem, eum?.',
+    },
+    {
+        question: 'Question 3',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda aspernatur, eligendi ipsam ratione natus ea cumque voluptatibus mollitia laborum, ab itaque ipsa qui suscipit a.Vel, totam error.Voluptatem, eum?.',
+    },
+    {
+        question: 'Question 4',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda aspernatur, eligendi ipsam ratione natus ea cumque voluptatibus mollitia laborum, ab itaque ipsa qui suscipit a.Vel, totam error.Voluptatem, eum?.',
+    },
+    {
+        question: 'Question 5',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda aspernatur, eligendi ipsam ratione natus ea cumque voluptatibus mollitia laborum, ab itaque ipsa qui suscipit a.Vel, totam error.Voluptatem, eum?.',
+    },
+    
+]
 
 export default FAQ;
