51c2664d26e9c8e4785ee437533a528f3a6b17e1
bugsyy157 <it.b.60.abhishek.yadav@gmail.com>
2022-10-30 01:40:59 +0530

    update

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import profile from "./images/profile.png"
+
 
 const MyAccount = () => {
     return (
-        <div>
-            <h1>Account Details</h1>
+       <>
+       <h1>MyAccount</h1><br />
+      <section className='accountDetails'>
+        <div className='profile-pic'><img src={profile} alt="pp" /></div>
+        <div className='details'>
+             
+                <label htmlFor="name">Name : {}</label><br />
+                <label htmlFor="seller_id">Seller Id : {}</label><br />
+                <label htmlFor="Address">Address : {}</label><br />
+                <label htmlFor="District">District : {}</label><br />
+                <label htmlFor="Pincode">Pincode : {}</label><br />
+                <label htmlFor="contact_details">Contact no : {}</label>
+             
         </div>
+      </section>
+      </>
     );
 };
 
